782dc9216baaff76a365c50bf0f0f8565c010501
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2021-12-31 17:34:27 +0100

    Lesson 40 & 41: Select Fields & Checkboxes

--- src/components/Signupform.vue
+++ src/components/Signupform.vue
@@ -4,9 +4,36 @@
     <input type="email" required v-model="email" />
     <label>Password</label>
     <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+    <!-- first way of doing checkboxes: using a single checkbox and boolean for whether it has been checked or not -->
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
+    <!-- second way of doing checkboxes: using multiple checkboxes and collecting the names in the array name-->
+    <!-- <div>
+      <input type="checkbox" value="shaun" v-model="name" />
+      <label>Shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yoshi" v-model="name" />
+      <label>yoshi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="mario" v-model="name" />
+      <label>mario</label>
+    </div> -->
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+  <!-- <p>Names: {{ name }}</p> -->
 </template>
 
 <script>
@@ -15,6 +42,9 @@ export default {
     return {
       email: "",
       password: "",
+      role: "",
+      terms: false,
+      name: [],
     };
   },
 };
@@ -38,7 +68,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -46,5 +77,12 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
